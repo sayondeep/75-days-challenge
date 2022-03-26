b8aac5b3d8e6d69f6b0f1dbfe7a4de4fc5afa022
sayondeep <sayondeep0710@rediffmail.com>
2022-03-26 18:53:43 +0530

    first problem

---
+++ Day1/two_sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<pair<int, int>> p;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            p.push_back({nums[i], i});
+        }
+        sort(p.begin(), p.end());
+
+        int i = 0;
+        int j = nums.size() - 1;
+        vector<int> ans;
+        while (i < j)
+        {
+            if (p[i].first + p[j].first < target)
+                i++;
+            if (p[i].first + p[j].first > target)
+                j--;
+            if (p[i].first + p[j].first == target)
+            {
+                //if(mp[nums[i]]!=mp[nums[j]])
+                //{
+                ans.push_back(p[i].second);
+                ans.push_back(p[j].second);
+                break;
+                //}
+            }
+        }
+
+        return ans;
+
+    }
+};
